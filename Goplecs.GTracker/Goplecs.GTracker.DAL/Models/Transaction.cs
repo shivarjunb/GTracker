eaa39bad8ccd492f6e46bf56c82e813f4d5c903a
ashutoshsigdel <34797684+ashutoshsigdel@users.noreply.github.com>
2017-12-24 14:55:36 -0600

    Added Category, ItemType, Mate, Transaction model

---
+++ Goplecs.GTracker/Goplecs.GTracker.DAL/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goplecs.GTracker.DAL
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public int GroceryId { get; set; }
+        public long GroceryOwnerId { get; set; }
+        public long PayeeId { get; set; }
+    }
+}
+
--- Goplecs.GTracker/Goplecs.GTracker.DAL/Models/User.cs
+++ Goplecs.GTracker/Goplecs.GTracker.DAL/Models/User.cs
@@ -13,6 +13,7 @@ namespace Goplecs.GTracker.DAL
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public Byte[] Photo { get; set; }
 
     }
 }
