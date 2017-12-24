ac8656daa6a6ed68340b56c7106f05bca89c10cb
shivarjun bhattarai <shivarjunb@users.noreply.github.com>
2017-12-24 13:57:04 -0600

    Added Models

---
+++ Goplecs.GTracker/Goplecs.GTracker.DAL/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goplecs.GTracker.DAL.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public double Amount { get; set; }
+        public long GroceryId { get; set; }
+        public long OwnerId { get; set; }
+        public long PayeeId { get; set; }
+
+    }
+}
